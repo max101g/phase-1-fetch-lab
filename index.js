f87d344f692acce2aae07dca9fe42d7b4f19ea48
max101g <maxgithinji101@gmail.com>
2022-12-06 23:32:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function (response){
+    console.log(response)
+    return response.json()
+  })
+  .then(function (fetchBooks){
+    console.log(fetchBooks)
+  })
   
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
